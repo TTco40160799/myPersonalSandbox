ad2fbaf30b2229903e256583f4da3f860e00cd57
badfatcat71 <badfatcat.biz@gmail.com>
2019-12-17 13:12:51 +0900

    create FW

--- createFW/hello.py
+++ createFW/hello.py
@@ -1,3 +1,11 @@
 def application(env, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'Hello World']
+    path = env['PATH_INFO']
+    if path == '/':
+        start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+        return [b'Hello World']
+    elif path == '/foo':
+        start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+        return [b'foo']
+    else:
+        start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+        return [b'404 Not Found']
