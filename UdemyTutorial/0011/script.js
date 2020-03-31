46d0dd9f55b91abf9495d27b3aea0c0e1f3fc0d7
T2 <badfatcat71@users.noreply.github.com>
2020-03-31 20:49:17 +0900

    update

--- UdemyTutorial/0011/script.js
+++ UdemyTutorial/0011/script.js
@@ -1,58 +1,1 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-
-// Show input error message
-function showError(input, message){
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-}
-
-// Show success outline
-function showSuccess(input){
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-}
-
-// Check email is valid
-function isValidEmail(email){
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-// Event Listeners
-form.addEventListener('submit', function(e) {
-    e.preventDefault(e);
-
-    if(username.value == ''){
-        showError(username, 'Username is required');
-    }else{
-        showSuccess(username);
-    }
-
-    if(email.value == ''){
-        showError(email, 'email is required');
-    }else if(isValidEmail(email.value)){
-        showError(email, 'email is not valid');
-    }else{
-        showSuccess(email);
-    }
-
-    if(password.value == ''){
-        showError(password, 'password is required');
-    }else{
-        showSuccess(password);
-    }
-
-    if(password2.value == ''){
-        showError(password2, 'Username is required');
-    }else{
-        showSuccess(password2);
-    }
-
-});
-
+const form = document.getElementById('form');const username = document.getElementById('username');const email = document.getElementById('email');const password = document.getElementById('password');const password2 = document.getElementById('password2');// Show input error messagefunction showError(input, message){    const formControl = input.parentElement;    formControl.className = 'form-control error';    const small = formControl.querySelector('small');    small.innerText = message;}// Show success outlinefunction showSuccess(input){    const formControl = input.parentElement;    formControl.className = 'form-control error';}// Check email is validfunction isValidEmail(email){    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;    return re.test(String(email).toLowerCase());}// Check Required Fieldfunction checkRequired(inputArr){    inputArr.forEach(function(input) {        if(input.value.trim() === ''){            showError(input, `${getFieldName(input)} is required`);        }else{            showSuccess(input);        }    });}// check Lengthfunction checkLength(input, min ,max){    if(input.value.length < min || input.length > max) {        showError(input, `${getFieldName(input)} must be between ${min} and ${max} characters`);    }else {        showSuccess(input);    }}// get field namefunction getFieldName(input){    return input.id.charAt(0).toUpperCase() + input.id.slice(1);}// Event Listenersform.addEventListener('submit', function(e) {    e.preventDefault(e);    checkRequired([username, email, password, password2]);    checkLength(username, 3, 15);    checkLength(password, 6, 25);});
